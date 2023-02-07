5b72a1fa4e1fb2f53e039cdeb6405cee9b9390df
chaovincent7 <117843673+chaovincent7@users.noreply.github.com>
2023-02-07 12:01:35 -0600

    Finished models, and some utility stuff, Working on Controller

---
+++ dollarbank/src/applcation/DollarBanksApplication.java
@@ -0,0 +1,16 @@
+package applcation;
+
+import java.util.Scanner;
+
+import model.Account;
+import model.Customer;
+
+public class DollarBanksApplication {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+
+}
---
+++ dollarbank/src/controller/DollarBanksController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import model.Account;
+import model.Customer;
+import utility.ColorsUtility;
+
+
+public class DollarBanksController {
+
+}
